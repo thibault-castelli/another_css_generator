7fa593f508434a68b25da4c5c5ee1554bda8d421
Thibault Castelli <thibault.castelli.tc@gmail.com>
2023-07-21 10:53:04 -0400

    Add links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,27 @@
+import { AiFillGithub } from 'react-icons/ai';
+
 const Footer = () => {
    return (
       <footer>
          <div className="container">
-            <h2>footer</h2>
+            <p>
+               made by
+               <a
+                  href="https://www.linkedin.com/in/thibault-castelli"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="underline-link"
+               >
+                  Thibault Castelli
+               </a>
+            </p>
+            <a
+               href="https://github.com/nineInchClous"
+               rel="noopener noreferrer"
+               target="_blank"
+            >
+               <AiFillGithub />
+            </a>
          </div>
       </footer>
    );
